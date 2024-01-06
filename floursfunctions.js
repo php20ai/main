e7100efa6a1bb29f313d21e9c7fb569c8688d43e
php20ai <aliah.iqbal123@hotmail.com>
2024-01-06 03:48:27 +0000

    Not allowing any negative value that are from the "Enter Value" field, to be the unput of the functions in this file.

--- floursfunctions.js
+++ floursfunctions.js
@@ -8,154 +8,155 @@ function floursFunction(){
     var floursinputone=document.getElementById("floursinputone").value;
     var parsfloatflours=parseFloat(floursinputone);
     
+    if (parsfloatflours>=0){
+        if (select1value=="flour") {
 
-    if (select1value=="flour") {
+            if (select2value=="grams" && select3value=="Cups") {
+                
+                document.getElementById("floursoutput").style.backgroundColor="bisque";
+                var output=0.01*(parsfloatflours);
+                var outputrounded=Math.round(output*100)/100;
 
-        if (select2value=="grams" && select3value=="Cups") {
-            
-            document.getElementById("floursoutput").style.backgroundColor="bisque";
-            var output=0.01*(parsfloatflours);
-            var outputrounded=Math.round(output*100)/100;
+                document.getElementById("floursoutput1").innerHTML = parsfloatflours + " g" +" = "+" " + outputrounded + " Cups" ;
 
-            document.getElementById("floursoutput1").innerHTML = parsfloatflours + " g" +" = "+" " + outputrounded + " Cups" ;
+            }
+
+            if (select2value=="Cups" && select3value=="grams") {
+                document.getElementById("floursoutput").style.backgroundColor="bisque";
+
+                var output=(parsfloatflours)/(0.01);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("floursoutput1").innerHTML = parsfloatflours + " Cups" +" = "+" " + outputrounded + " g" ;
+
+            }
+
+
+            if (select2value=="grams" && select3value=="Tablespoon") {
+                document.getElementById("floursoutput").style.backgroundColor="bisque";
+
+                var output=(0.128)*(parsfloatflours);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("floursoutput1").innerHTML = parsfloatflours + " g" +" = "+" " + outputrounded + " tablespoon (tbsp)" 
+
+            }
+
+            if (select2value=="Tablespoon" && select3value=="grams") {
+                document.getElementById("floursoutput").style.backgroundColor="bisque";
+
+                var output=(parsfloatflours)/(0.128);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("floursoutput1").innerHTML = parsfloatflours + " tablespoon (tbsp)" +" = "+" " + outputrounded + " g"; 
+
+            }
+
+            if (select2value=="Cups" && select3value=="Tablespoon") {
+                document.getElementById("floursoutput").style.backgroundColor="bisque";
+
+                var output=16*(parsfloatflours);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("floursoutput1").innerHTML = parsfloatflours + " Cups" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
+
+            }
+
+            if (select2value=="Tablespoon" && select3value=="Cups") {
+                document.getElementById("floursoutput").style.backgroundColor="bisque";
+
+                var output=(parsfloatflours)/(16);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("floursoutput1").innerHTML = parsfloatflours + " tablespoon (tbsp)" +" = "+" " + outputrounded + " Cups" 
+
+            }
+
+        
+
+
+
 
         }
 
-        if (select2value=="Cups" && select3value=="grams") {
-            document.getElementById("floursoutput").style.backgroundColor="bisque";
+        if (select1value=="oats") {
 
-            var output=(parsfloatflours)/(0.01);
+            if (select2value=="grams" && select3value=="Cups") {
+                document.getElementById("floursoutput").style.backgroundColor="bisque";
 
-            var outputrounded=Math.round(output*100)/100;
+                var output=(parsfloatflours)/(80);
 
-            document.getElementById("floursoutput1").innerHTML = parsfloatflours + " Cups" +" = "+" " + outputrounded + " g" ;
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("floursoutput1").innerHTML = parsfloatflours + " g" +" = "+" " + outputrounded + " Cups";
+
+            }
+
+            if (select2value=="Cups" && select3value=="grams") {
+                document.getElementById("floursoutput").style.backgroundColor="bisque";
+                var output=80*(parsfloatflours);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("floursoutput1").innerHTML = parsfloatflours + " Cups" +" = "+" " + outputrounded + " g";
+            }
+
+
+            if (select2value=="grams" && select3value=="Tablespoon") {
+
+                document.getElementById("floursoutput").style.backgroundColor="bisque";
+
+                var output=(parsfloatflours)/(10);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("floursoutput1").innerHTML = parsfloatflours + " g" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
+
+            }
+
+            if (select2value=="Tablespoon" && select3value=="grams") {
+                document.getElementById("floursoutput").style.backgroundColor="bisque";
+
+                var output=(parsfloatflours)*(10);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("floursoutput1").innerHTML = parsfloatflours + " tablespoon (tbsp)" +" = "+" " + outputrounded + " g";
+
+            }
+
+            if (select2value=="Cups" && select3value=="Tablespoon") {
+                document.getElementById("floursoutput").style.backgroundColor="bisque";
+
+                var output=8*(parsfloatflours);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("floursoutput1").innerHTML = parsfloatflours + " Cups" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
+
+            }
+
+            if (select2value=="Tablespoon" && select3value=="Cups") {
+                document.getElementById("floursoutput").style.backgroundColor="bisque";
+
+                var output=(parsfloatflours)/(8);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("floursoutput1").innerHTML = parsfloatflours + " tablespoon (tbsp)" +" = "+" " + outputrounded + " Cups";
+
+            }
+
+
+
 
         }
-
-
-        if (select2value=="grams" && select3value=="Tablespoon") {
-            document.getElementById("floursoutput").style.backgroundColor="bisque";
-
-            var output=(0.128)*(parsfloatflours);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("floursoutput1").innerHTML = parsfloatflours + " g" +" = "+" " + outputrounded + " tablespoon (tbsp)" 
-
-        }
-
-        if (select2value=="Tablespoon" && select3value=="grams") {
-            document.getElementById("floursoutput").style.backgroundColor="bisque";
-
-            var output=(parsfloatflours)/(0.128);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("floursoutput1").innerHTML = parsfloatflours + " tablespoon (tbsp)" +" = "+" " + outputrounded + " g"; 
-
-        }
-
-        if (select2value=="Cups" && select3value=="Tablespoon") {
-            document.getElementById("floursoutput").style.backgroundColor="bisque";
-
-            var output=16*(parsfloatflours);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("floursoutput1").innerHTML = parsfloatflours + " Cups" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
-
-        }
-
-        if (select2value=="Tablespoon" && select3value=="Cups") {
-            document.getElementById("floursoutput").style.backgroundColor="bisque";
-
-            var output=(parsfloatflours)/(16);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("floursoutput1").innerHTML = parsfloatflours + " tablespoon (tbsp)" +" = "+" " + outputrounded + " Cups" 
-
-        }
-
-       
-
-
-
-
+        
+        
     }
-
-    if (select1value=="oats") {
-
-        if (select2value=="grams" && select3value=="Cups") {
-            document.getElementById("floursoutput").style.backgroundColor="bisque";
-
-            var output=(parsfloatflours)/(80);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("floursoutput1").innerHTML = parsfloatflours + " g" +" = "+" " + outputrounded + " Cups";
-
-        }
-
-        if (select2value=="Cups" && select3value=="grams") {
-            document.getElementById("floursoutput").style.backgroundColor="bisque";
-            var output=80*(parsfloatflours);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("floursoutput1").innerHTML = parsfloatflours + " Cups" +" = "+" " + outputrounded + " g";
-        }
-
-
-        if (select2value=="grams" && select3value=="Tablespoon") {
-
-            document.getElementById("floursoutput").style.backgroundColor="bisque";
-
-            var output=(parsfloatflours)/(10);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("floursoutput1").innerHTML = parsfloatflours + " g" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
-
-        }
-
-        if (select2value=="Tablespoon" && select3value=="grams") {
-            document.getElementById("floursoutput").style.backgroundColor="bisque";
-
-            var output=(parsfloatflours)*(10);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("floursoutput1").innerHTML = parsfloatflours + " tablespoon (tbsp)" +" = "+" " + outputrounded + " g";
-
-        }
-
-        if (select2value=="Cups" && select3value=="Tablespoon") {
-            document.getElementById("floursoutput").style.backgroundColor="bisque";
-
-            var output=8*(parsfloatflours);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("floursoutput1").innerHTML = parsfloatflours + " Cups" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
-
-        }
-
-        if (select2value=="Tablespoon" && select3value=="Cups") {
-            document.getElementById("floursoutput").style.backgroundColor="bisque";
-
-            var output=(parsfloatflours)/(8);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("floursoutput1").innerHTML = parsfloatflours + " tablespoon (tbsp)" +" = "+" " + outputrounded + " Cups";
-
-        }
-
-
-
-
-    }
-    
-    
 }
