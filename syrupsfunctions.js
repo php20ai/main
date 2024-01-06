8c09f41855386b9c7ed02a15dc4b0dfee768f270
php20ai <aliah.iqbal123@hotmail.com>
2024-01-06 03:53:59 +0000

    Not allowing any negative value from the "Enter Value" field to be the input of any of the functions in this file.

--- syrupsfunctions.js
+++ syrupsfunctions.js
@@ -8,143 +8,144 @@ function syrupsFunction(){
     var syrupsinputone=document.getElementById("syrupsinputone").value;
     var parsfloatsyrups=parseFloat(syrupsinputone);
     
+    if (parsfloatsyrups>=0){
+        if (select1value=="Golden Syrup") {
 
-    if (select1value=="Golden Syrup") {
+            if (select2value=="grams" && select3value=="Cups") {
+                document.getElementById("syrupsoutput").style.backgroundColor="bisque";
 
-        if (select2value=="grams" && select3value=="Cups") {
-            document.getElementById("syrupsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatsyrups)/(350);
+                var outputrounded=Math.round(output*100)/100;
 
-            var output=(parsfloatsyrups)/(350);
-            var outputrounded=Math.round(output*100)/100;
+                document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " g" +" = "+" " + outputrounded + " Cups" ;
 
-            document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " g" +" = "+" " + outputrounded + " Cups" ;
+            }
 
-        }
+            if (select2value=="Cups" && select3value=="grams") {
+                document.getElementById("syrupsoutput").style.backgroundColor="bisque";
+                var output=(350)*(parsfloatsyrups);
 
-        if (select2value=="Cups" && select3value=="grams") {
-            document.getElementById("syrupsoutput").style.backgroundColor="bisque";
-            var output=(350)*(parsfloatsyrups);
+                var outputrounded=Math.round(output*100)/100;
 
-            var outputrounded=Math.round(output*100)/100;
+                document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " Cups" +" = "+" " + outputrounded + " g" ;
 
-            document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " Cups" +" = "+" " + outputrounded + " g" ;
-
-        }
-
-
-        if (select2value=="grams" && select3value=="Tablespoon") {
-            document.getElementById("syrupsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatsyrups)/(21.9);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " g" +" = "+" " + outputrounded + " tablespoon (tbsp)";
-
-        }
-
-        if (select2value=="Tablespoon" && select3value=="grams") {
-            document.getElementById("syrupsoutput").style.backgroundColor="bisque";
-            var output=(21.9)*(parsfloatsyrups);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " tablespoon (tbsp)" +" = "+" " + outputrounded + " g"; 
-
-        }
-
-        if (select2value=="Cups" && select3value=="Tablespoon") {
-            document.getElementById("syrupsoutput").style.backgroundColor="bisque";
-            var output=(350/(21.9))*(parsfloatsyrups);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " Cups" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
-
-        }
-
-        if (select2value=="Tablespoon" && select3value=="Cups") {
-            document.getElementById("syrupsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatsyrups)/(350/(21.9));
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " tablespoon (tbsp)" +" = "+" " + outputrounded + " Cups" 
-
-        }
-
-       
+            }
 
 
+            if (select2value=="grams" && select3value=="Tablespoon") {
+                document.getElementById("syrupsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatsyrups)/(21.9);
 
+                var outputrounded=Math.round(output*100)/100;
 
-    }
+                document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " g" +" = "+" " + outputrounded + " tablespoon (tbsp)";
 
-    if (select1value=="Honey") {
+            }
+
+            if (select2value=="Tablespoon" && select3value=="grams") {
+                document.getElementById("syrupsoutput").style.backgroundColor="bisque";
+                var output=(21.9)*(parsfloatsyrups);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " tablespoon (tbsp)" +" = "+" " + outputrounded + " g"; 
+
+            }
+
+            if (select2value=="Cups" && select3value=="Tablespoon") {
+                document.getElementById("syrupsoutput").style.backgroundColor="bisque";
+                var output=(350/(21.9))*(parsfloatsyrups);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " Cups" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
+
+            }
+
+            if (select2value=="Tablespoon" && select3value=="Cups") {
+                document.getElementById("syrupsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatsyrups)/(350/(21.9));
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " tablespoon (tbsp)" +" = "+" " + outputrounded + " Cups" 
+
+            }
+
         
-        if (select2value=="grams" && select3value=="Cups") {
-            document.getElementById("syrupsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatsyrups)/(340);
 
-            var outputrounded=Math.round(output*100)/100;
 
-            document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " g" +" = "+" " + outputrounded + " Cups";
+
 
         }
 
-        if (select2value=="Cups" && select3value=="grams") {
-            document.getElementById("syrupsoutput").style.backgroundColor="bisque";
-            var output=(340)*(parsfloatsyrups);
+        if (select1value=="Honey") {
+            
+            if (select2value=="grams" && select3value=="Cups") {
+                document.getElementById("syrupsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatsyrups)/(340);
 
-            var outputrounded=Math.round(output*100)/100;
+                var outputrounded=Math.round(output*100)/100;
 
-            document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " Cups" +" = "+" " + outputrounded + " g";
+                document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " g" +" = "+" " + outputrounded + " Cups";
+
+            }
+
+            if (select2value=="Cups" && select3value=="grams") {
+                document.getElementById("syrupsoutput").style.backgroundColor="bisque";
+                var output=(340)*(parsfloatsyrups);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " Cups" +" = "+" " + outputrounded + " g";
+            }
+
+
+            if (select2value=="grams" && select3value=="Tablespoon") {
+                document.getElementById("syrupsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatsyrups)/(20.4);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " g" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
+
+            }
+
+            if (select2value=="Tablespoon" && select3value=="grams") {
+                document.getElementById("syrupsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatsyrups)*(20.4);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " tablespoon (tbsp)" +" = "+" " + outputrounded + " g";
+
+            }
+
+            if (select2value=="Cups" && select3value=="Tablespoon") {
+                document.getElementById("syrupsoutput").style.backgroundColor="bisque";
+                var output=(340/(20.4))*(parsfloatsyrups);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " Cups" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
+
+            }
+
+            if (select2value=="Tablespoon" && select3value=="Cups") {
+                document.getElementById("syrupsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatsyrups)/(340/(20.4));
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " tablespoon (tbsp)" +" = "+" " + outputrounded + " Cups";
+
+            }
+
+
+
+
         }
-
-
-        if (select2value=="grams" && select3value=="Tablespoon") {
-            document.getElementById("syrupsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatsyrups)/(20.4);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " g" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
-
-        }
-
-        if (select2value=="Tablespoon" && select3value=="grams") {
-            document.getElementById("syrupsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatsyrups)*(20.4);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " tablespoon (tbsp)" +" = "+" " + outputrounded + " g";
-
-        }
-
-        if (select2value=="Cups" && select3value=="Tablespoon") {
-            document.getElementById("syrupsoutput").style.backgroundColor="bisque";
-            var output=(340/(20.4))*(parsfloatsyrups);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " Cups" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
-
-        }
-
-        if (select2value=="Tablespoon" && select3value=="Cups") {
-            document.getElementById("syrupsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatsyrups)/(340/(20.4));
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("syrupsoutput1").innerHTML = parsfloatsyrups + " tablespoon (tbsp)" +" = "+" " + outputrounded + " Cups";
-
-        }
-
-
-
-
+        
+        
     }
-    
-    
 }
