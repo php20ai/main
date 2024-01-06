7df58d0c37f5dc9a7df9ec484a18e139af8ab1e4
php20ai <aliah.iqbal123@hotmail.com>
2024-01-06 03:55:50 +0000

    Not allowing any negative value from the "Enter Value" field to be the input of any of the functions in this file.

--- buttersoilsfunctions.js
+++ buttersoilsfunctions.js
@@ -8,143 +8,144 @@ function buttersoilsFunction(){
     var buttersoilsinputone=document.getElementById("buttersoilsinputone").value;
     var parsfloatbuttersoils=parseFloat(buttersoilsinputone);
     
+    if (parsfloatbuttersoils>=0){
+        if (select1value=="butter") {
 
-    if (select1value=="butter") {
+            if (select2value=="grams" && select3value=="Cups") {
+                
+                document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatbuttersoils)/(226);
+                var outputrounded=Math.round(output*100)/100;
 
-        if (select2value=="grams" && select3value=="Cups") {
-            
-            document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatbuttersoils)/(226);
-            var outputrounded=Math.round(output*100)/100;
+                document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " g" +" = "+" " + outputrounded + " Cups" ;
 
-            document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " g" +" = "+" " + outputrounded + " Cups" ;
+            }
+
+            if (select2value=="Cups" && select3value=="grams") {
+                document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatbuttersoils)*(226);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " Cups" +" = "+" " + outputrounded + " g" ;
+
+            }
+
+
+            if (select2value=="grams" && select3value=="Tablespoon") {
+                document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatbuttersoils)/(13.6);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " g" +" = "+" " + outputrounded + " tablespoon (tbsp)";
+
+            }
+
+            if (select2value=="Tablespoon" && select3value=="grams") {
+                document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatbuttersoils)*(13.6);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " tablespoon (tbsp)" +" = "+" " + outputrounded + " g"; 
+
+            }
+
+            if (select2value=="Cups" && select3value=="Tablespoon") {
+                document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
+                var output=(226/(13.6))*(parsfloatbuttersoils);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " Cups" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
+
+            }
+
+            if (select2value=="Tablespoon" && select3value=="Cups") {
+                document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatbuttersoils)/(226/(13.6));
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " tablespoon (tbsp)" +" = "+" " + outputrounded + " Cups"; 
+
+            }
+
+        
+
+
+
 
         }
 
-        if (select2value=="Cups" && select3value=="grams") {
-            document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatbuttersoils)*(226);
+        if (select1value=="oil") {
 
-            var outputrounded=Math.round(output*100)/100;
+            if (select2value=="grams" && select3value=="Cups") {
+                document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatbuttersoils)/(224);
 
-            document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " Cups" +" = "+" " + outputrounded + " g" ;
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " g" +" = "+" " + outputrounded + " Cups";
+
+            }
+
+            if (select2value=="Cups" && select3value=="grams") {
+                document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
+                var output=(224)*(parsfloatbuttersoils);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " Cups" +" = "+" " + outputrounded + " g";
+            }
+
+
+            if (select2value=="grams" && select3value=="Tablespoon") {
+                document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatbuttersoils)/(13.5);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " g" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
+
+            }
+
+            if (select2value=="Tablespoon" && select3value=="grams") {
+                document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatbuttersoils)*(13.5);
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " tablespoon (tbsp)" +" = "+" " + outputrounded + " g";
+
+            }
+
+            if (select2value=="Cups" && select3value=="Tablespoon") {
+                document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatbuttersoils)*(224/(13.5));
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " Cups" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
+
+            }
+
+            if (select2value=="Tablespoon" && select3value=="Cups") {
+                document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
+                var output=(parsfloatbuttersoils)/(224/(13.5));
+
+                var outputrounded=Math.round(output*100)/100;
+
+                document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " tablespoon (tbsp)" +" = "+" " + outputrounded + " Cups";
+
+            }
+
+
+
 
         }
-
-
-        if (select2value=="grams" && select3value=="Tablespoon") {
-            document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatbuttersoils)/(13.6);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " g" +" = "+" " + outputrounded + " tablespoon (tbsp)";
-
-        }
-
-        if (select2value=="Tablespoon" && select3value=="grams") {
-            document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatbuttersoils)*(13.6);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " tablespoon (tbsp)" +" = "+" " + outputrounded + " g"; 
-
-        }
-
-        if (select2value=="Cups" && select3value=="Tablespoon") {
-            document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
-            var output=(226/(13.6))*(parsfloatbuttersoils);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " Cups" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
-
-        }
-
-        if (select2value=="Tablespoon" && select3value=="Cups") {
-            document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatbuttersoils)/(226/(13.6));
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " tablespoon (tbsp)" +" = "+" " + outputrounded + " Cups"; 
-
-        }
-
-       
-
-
-
-
+        
+        
     }
-
-    if (select1value=="oil") {
-
-        if (select2value=="grams" && select3value=="Cups") {
-            document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatbuttersoils)/(224);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " g" +" = "+" " + outputrounded + " Cups";
-
-        }
-
-        if (select2value=="Cups" && select3value=="grams") {
-            document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
-            var output=(224)*(parsfloatbuttersoils);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " Cups" +" = "+" " + outputrounded + " g";
-        }
-
-
-        if (select2value=="grams" && select3value=="Tablespoon") {
-            document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatbuttersoils)/(13.5);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " g" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
-
-        }
-
-        if (select2value=="Tablespoon" && select3value=="grams") {
-            document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatbuttersoils)*(13.5);
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " tablespoon (tbsp)" +" = "+" " + outputrounded + " g";
-
-        }
-
-        if (select2value=="Cups" && select3value=="Tablespoon") {
-            document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatbuttersoils)*(224/(13.5));
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " Cups" +" = "+" " + outputrounded + " tablespoon (tbsp)"; 
-
-        }
-
-        if (select2value=="Tablespoon" && select3value=="Cups") {
-            document.getElementById("buttersoilsoutput").style.backgroundColor="bisque";
-            var output=(parsfloatbuttersoils)/(224/(13.5));
-
-            var outputrounded=Math.round(output*100)/100;
-
-            document.getElementById("buttersoilsoutput1").innerHTML = parsfloatbuttersoils + " tablespoon (tbsp)" +" = "+" " + outputrounded + " Cups";
-
-        }
-
-
-
-
-    }
-    
-    
 }
